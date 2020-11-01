2c242c3aa876513cf6d2544f578bfb6897f6dcc6
khontharos <khontharos.bb@gmail.com>
2020-11-01 16:28:45 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,128 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screen/HomeScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import PostScreen from "./screens/PostScreen";
+import MessageScreen from "./screens/MessageScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import * as firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyCSyHWDmm-sxSRMZ6Y6DILo_6lUgDllGik",
+  authDomain: "socialapp-ceb80.firebaseapp.com",
+  databaseURL: "https://socialapp-ceb80.firebaseio.com",
+  projectId: "socialapp-ceb80",
+  storageBucket: "socialapp-ceb80.appspot.com",
+  messagingSenderId: "361503611572",
+  appId: "1:361503611572:web:901109e1c9f20da97155e1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const AppContainer = createStackNavigator(
+  {
+    default: createBottomTabNavigator(
+      {
+        Home: {
+          screen: HomeScreen,
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Ionicons name="ios-home" size={24} color={tintColor} />
+            ),
+          },
+        },
+        Message: {
+          screen: MessageScreen,
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Ionicons name="ios-chatboxes" size={24} color={tintColor} />
+            ),
+          },
+        },
+        Post: {
+          screen: PostScreen,
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Ionicons
+                name="ios-add-circle"
+                size={48}
+                color="#E9446A"
+                style={{
+                  shadowColor: "#E9446A",
+                  shadowOffset: { width: 0, height: 0 },
+                  shadowRadius: 10,
+                  shadowOpacity: 0.3,
+                }}
+              />
+            ),
+          },
+        },
+        Notification: {
+          screen: NotificationScreen,
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Ionicons name="ios-notifications" size={24} color={tintColor} />
+            ),
+          },
+        },
+        Profile: {
+          screen: ProfileScreen,
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Ionicons name="ios-person" size={24} color={tintColor} />
+            ),
+          },
+        },
+      },
+      {
+        defaultNavigationOptions: {
+          tabBarOnPress: ({ navigation, defaultHandler }) => {
+            if (navigation.state.key === "Post") {
+              navigation.navigate("postModal");
+            } else {
+              defaultHandler();
+            }
+          },
+        },
+        tabBarOptions: {
+          activeTintColor: "#161F3D",
+          inactiveTintColor: "#B8BBC4",
+          showLabel: false,
+        },
+      }
+    ),
+    postModal: {
+      screen: PostScreen,
+    },
   },
-});
+  {
+    mode: "modal",
+    headerMode: "none",
+    // initialRouteName: "postModal"
+  }
+);
+const AuthStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen,
+  },
+  {
+    initialRouteName: "Register",
+  }
+);
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppContainer,
+      Auth: AuthStack,
+    },
+    {
+      initialRouteName: "Loading",
+    }
+  )
+);
